77bdb02733ce47b1d7a93e055088de8b4fc09d8e
brunarodrigues13 <brunacris.rodrigues@gmail.com>
2022-08-24 18:50:10 -0300

    realizando exercicios

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,49 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui.
+function compareTrue(value1, value2) {
+  if (value1 && value2 === true) {
+    return true;
+  } return false;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * altura / 2);
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+  split = splitSentence.split()
+  return split;
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+  return concatName.length -1 + concatName[0];
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let numMaior = array[0];
+  let index = 0;
+  let count = 0;
+
+  for (let index in array) {
+      if (array[index] > numMaior) {
+          numMaior = array[index];
+      }
+  }
+  for (let index2 in array) {
+      if (array[index2] === numMaior) {
+          count += 1;
+      }
+  }
+  
+  return count;
 }
 
 // Desafio 7
